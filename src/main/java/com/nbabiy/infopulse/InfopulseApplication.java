7481d2400846a90810af57b6efc258451e975087
Nazarii Babii <n.babiy69@gmail.com>
2023-04-08 20:42:59 +0300

    [NO-JIRA] Fix not added files to git

---
+++ src/main/java/com/nbabiy/infopulse/InfopulseApplication.java
@@ -0,0 +1,13 @@
+package com.nbabiy.infopulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfopulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfopulseApplication.class, args);
+    }
+
+}
